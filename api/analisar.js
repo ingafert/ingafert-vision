af718a4b134b8103f4f24cbc93e0f4315417a165
ingafert <contatoingafert@gmail.com>
2026-07-22 10:54:13 -0300

    Update analisar.js

--- api/analisar.js
+++ api/analisar.js
@@ -6,11 +6,15 @@ const openai = new OpenAI({
 
 export default async function handler(req, res) {
 
-  if (req.method !== "POST") {
-    return res.status(405).json({
-      erro: "Método não permitido"
+ if (req.method === "GET") {
+
+    return res.status(200).json({
+        status: "ok",
+        openai: !!process.env.OPENAI_API_KEY,
+        mensagem: "API pronta para receber imagens."
     });
-  }
+
+}
 
   try {
 
