a0cbad9ab757288a8ddef93f9b9cb5376ce7fc32
ingafert <contatoingafert@gmail.com>
2026-07-22 14:00:48 -0300

    Update analisar.js

--- api/analisar.js
+++ api/analisar.js
@@ -21,7 +21,13 @@ try {
 }
 
 export default async function handler(req, res) {
+res.setHeader("Access-Control-Allow-Origin", "https://www.ingafert.com.br");
+res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+if (req.method === "OPTIONS") {
+    return res.status(200).end();
+}
   if (req.method !== "POST") {
     return res.status(200).json({
       status: "ok",
