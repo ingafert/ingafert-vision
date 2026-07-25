52ce81e28a20953fb63552e2f8e75e2e16575486
ingafert <contatoingafert@gmail.com>
2026-07-25 12:20:46 -0300

    Update catalogo.js

--- api/catalogo.js
+++ api/catalogo.js
@@ -1,18 +1,58 @@
+import fs from "fs";
+import path from "path";
+import XLSX from "xlsx";
+
 export default async function handler(req, res) {
 
-    return res.status(200).json({
+    try {
 
-        produtos: [
+        const arquivo = path.join(process.cwd(), "dados", "produtos.xlsx");
 
-            {
-                referencia: "CQ32379",
-                nome: "Lâmina de Corte de Plataforma",
-                url: "https://ingafert.com.br/25-un-lamina-de-corte-vv-para-john-deere-refcq32379/p",
-                foto: ""
-            }
+        const workbook = XLSX.readFile(arquivo);
 
-        ]
+        const sheet = workbook.Sheets[workbook.SheetNames[0]];
 
-    });
+        const dados = XLSX.utils.sheet_to_json(sheet);
+
+        const produtos = dados.map(item => ({
+
+            referencia:
+                item.codigo_erp ||
+                item.referencia ||
+                "",
+
+            nome:
+                item.nome ||
+                "",
+
+            marca:
+                item.marca ||
+                "",
+
+            url:
+                item.link_produto ||
+                "",
+
+            foto:
+                item.link_foto_principal ||
+                "",
+
+            termos:
+                item.termos_de_busca ||
+                ""
+
+        }));
+
+        res.status(200).json({
+            produtos
+        });
+
+    } catch (erro) {
+
+        res.status(500).json({
+            erro: erro.message
+        });
+
+    }
 
 }
