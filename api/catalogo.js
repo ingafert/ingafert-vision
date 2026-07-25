e2a3070826c9211ed007c6a3e660f81f8f3e3448
ingafert <contatoingafert@gmail.com>
2026-07-25 12:05:29 -0300

    Update catalogo.js

--- api/catalogo.js
+++ api/catalogo.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
             {
                 referencia: "H20357",
                 nome: "Lâmina de Corte de Plataforma",
-                url: "https://www.ingafert.com.br/produto/lamina-de-corte-de-plataforma",
+                url: "https://ingafert.com.br/25-un-lamina-de-corte-vv-para-john-deere-refcq32379/p",
                 foto: ""
             }
 
